f99b250ec1e79fa421620d948368d4654aa82cf3
my name is bond <43314247+8OND007@users.noreply.github.com>
2026-08-16 11:25:35 +0200

    Update run.sh to include custom startup messages

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set -e
 
+echo "=============================="
+echo "CUSTOM FUXA RUN.SH IS RUNNING"
+echo "=============================="
+
+cat /data/options.json
+
 BASE_PATH="$(node -p "require('/data/options.json').BASE_PATH")"
 
-export BASE_PATH
-
-echo "=================================="
-echo "FUXA startup"
 echo "BASE_PATH=${BASE_PATH}"
-echo "=================================="
+
+export BASE_PATH
 
 exec /usr/local/bin/docker-entrypoint.sh node main.js
